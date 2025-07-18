406a65c84b7daa393d8d447ab6279801100cbfa2
pranavsinghpatil <puneet.pranav04@gmail.com>
2025-07-18 13:20:40 +0530

    reconfigure

--- h.py
+++ h.py
@@ -1,1 +1,14 @@
-print("hello world")
+def mergeAlternately( word1: str, word2: str) -> str:
+    word1 = word1
+    word2 = word2
+    r = len(word1)  + len(word2)
+    t = None
+    f = "one"
+    for i in range(3):
+        t = word1[i] + word2[i]
+        f = f + t
+    f = f[3:]
+
+    return f
+
+print(mergeAlternately(word1="abc", word2="pqr"))
